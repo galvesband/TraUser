4b107fd11562fefd2f6a163373ee5e9aa2ddeb03
Rafael Gálvez-Cañero <galvesband@gmail.com>
2016-09-20 20:23:57 +0200

    FIX a ruta de user_block, estirpar todo de UserCRUDController

--- Controller/UserCRUDController.php
+++ Controller/UserCRUDController.php
@@ -2,20 +2,7 @@
 
 namespace Galvesband\TraUserBundle\Controller;
 
-use Exception;
-use Galvesband\TraUserBundle\Entity\User;
 use Sonata\AdminBundle\Controller\CRUDController;
-use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserCRUDController extends CRUDController {
-    // TODO: createAction
-    //   Si usuario logeao no es ROLE_SUPER_ADMIN y algún grupo de los del nuevo usuario es ROLE_SUPER_ADMIN -> throw
-    // TODO: editAction o update o lo que sea
-    //   Si usuario logeao no es ROLE_SUPER_ADMIN y el usuario editado sí lo es -> Unauthorized
-    //   Si usuario logeao no es ROLE_SUPER_ADMIN y entre los grupos del usuario editado esta ROLE_SUPER_ADMIN -> throw
-
-    // TODO: Change password
 }
